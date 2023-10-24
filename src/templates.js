86e3b373a6096ae77ab9f7a4a502cc9848e07fd3
mi-skam <maksim@miskam.xyz>
2023-10-25 01:46:52 +0200

    adding new-objects/template-bun

--- src/templates.js
+++ src/templates.js
@@ -2,5 +2,6 @@ export const templateRepos = {
   'vanilla ': 'new-objects/template-vite-vanilla',
   'vanilla-tailwindcss': 'new-objects/template-vite-vanilla-tailwind',
   'vanilla-pixi5': 'new-objects/template-vite-pixi5',
+  'bun (backend)': 'new-objects/template-bun',
   'nodejs (backend)': 'new-objects/template-nodejs',
 };
