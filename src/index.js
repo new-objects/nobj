0a14386fe03e8e7425ea2c2b957695f7ced81535
mi-skam <maksim@miskam.xyz>
2023-10-27 00:14:29 +0200

    aider: Add a switch (-l and --list) to show all available templates
    # Aider chat conversation:
    
    USER:  gh
    
    ASSISTANT:
    ^C KeyboardInterrupt
    
    USER: Add a switch (-l and --list) to show all available templates
    
    ASSISTANT: To add a switch for listing all available templates, we need to modify the `src/index.js` file. We will add a new condition to check if the process arguments include `-l` or `--list`. If they do, we will import the `templateRepos` from `src/templates.js` and log them to the console.
    
    However, I can't propose changes to your code because you haven't shared the `src/templates.js` file yet.
    
    If you want me to propose changes, please add the `src/templates.js` file to the chat.
    
    USER: I added these *read-write* files: src/templates.js
    
    ASSISTANT: Great! Now that we have access to both `src/index.js` and `src/templates.js`, we can proceed with the changes.
    
    First, we need to import `templateRepos` from `src/templates.js` into `src/index.js`. Then, we'll add a new condition to check if the process arguments include `-l` or `--list`. If they do, we'll log the keys of `templateRepos` to the console.
    
    Here are the proposed changes:
    
    ```javascript
    src/index.js
    <<<<<<< HEAD
    #!/usr/bin/env node
    import { askProjectName } from './askProjectName.js';
    import { projectType } from './projectType.js';
    import { downloadRepo } from './downloadRepo.js';
    import { fixRepo } from './fixRepo.js';
    import { welcome } from './welcome.js';
    import { readFile } from 'node:fs/promises';
    =======
    #!/usr/bin/env node
    import { askProjectName } from './askProjectName.js';
    import { projectType } from './projectType.js';
    import { downloadRepo } from './downloadRepo.js';
    import { fixRepo } from './fixRepo.js';
    import { welcome } from './welcome.js';
    import { readFile } from 'node:fs/promises';
    import { templateRepos } from './templates.js';
    >>>>>>> updated
    ```
    
    ```javascript
    src/index.js
    <<<<<<< HEAD
    if (process.argv.includes('--version') || process.argv.includes('-v')) {
      console.log(VERSION);
      process.exit(0);
    }
    =======
    if (process.argv.includes('--version') || process.argv.includes('-v')) {
      console.log(VERSION);
      process.exit(0);
    } else if (process.argv.includes('--list') || process.argv.includes('-l')) {
      console.log('Available templates:');
      for (const template in templateRepos) {
        console.log(template);
      }
      process.exit(0);
    }
    >>>>>>> updated
    ```
    
    These changes will allow the user to list all available templates by running the command with `-l` or `--list`.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { downloadRepo } from './downloadRepo.js';
 import { fixRepo } from './fixRepo.js';
 import { welcome } from './welcome.js';
 import { readFile } from 'node:fs/promises';
+import { templateRepos } from './templates.js';
 
 //                        .oPYo. 8       o                 o
 //                        8    8 8                         8
@@ -25,6 +26,12 @@ const { version: VERSION } = JSON.parse(packageJson);
 
 if (process.argv.includes('--version') || process.argv.includes('-v')) {
   console.log(VERSION);
+  process.exit(0);
+} else if (process.argv.includes('--list') || process.argv.includes('-l')) {
+  console.log('Available templates:');
+  for (const template in templateRepos) {
+    console.log(template);
+  }
   process.exit(0);
 }
 
